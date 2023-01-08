02396d7fe7fa06bc409e282a5687a9a832ee1266
newway-anshul <itsaj123@gmail.com>
2023-01-08 10:30:46 +0530

    added tx form

--- src/app/components/send-trasaction/send-trasaction.component.ts
+++ src/app/components/send-trasaction/send-trasaction.component.ts
@@ -10,9 +10,10 @@ export class SendTrasactionComponent implements OnInit {
   //heading variables
   network = 'Test Network';
   knowMore = '(know more)';
+  toAddresses = ['0x0', '0x1', '0x2'];
   sendTxForm = new FormGroup({
     from: new FormControl('0x'),
-    to: new FormControl('0x'),
+    to: new FormControl(this.toAddresses[0]),
     amnt: new FormControl('0'),
     gasFee: new FormControl('0'),
   });
